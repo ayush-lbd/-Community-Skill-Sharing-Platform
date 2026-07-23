ceffd7c993fca0726ca84ec2267cadb0d57d139d
ayushpandey <kumarpandeyayush366@gmail.com>
2026-07-23 21:10:06 +0530

    add user registration route and update server configuration

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
 
+
 const app=express()
 
 app.use(cors({
@@ -13,5 +14,14 @@ app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+app.get("/test", (req, res) => {
+    res.status(200).json({ message: "Express is alive on port 8000!" });
+});
+// Routes import
+import userRouter from "./routes/user.routes.js"
+
+// Routes declaration
+app.use("/api/users", userRouter)
 
 export { app }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 //import dotenv from "dotenv"
+import dotenv from "dotenv";
+dotenv.config({
+    path: "./.env"
+});
 import connectDB from "./db/index.js"
-import { app } from "./app.js"
+
 // dotenv.config({
 //     path: ".env"
 // })
-
-
+import { app } from "./app.js"
 connectDB()
 .then(() => {
     
-        const PORT = process.env.PORT || 4000
+        const PORT = process.env.PORT || 8000
         app.listen(PORT, () => {
-            console.log(` Server is running on port ${PORT}`);
+            console.log(` Server is beautifully running on port ${PORT}`);
         });
     
 })
